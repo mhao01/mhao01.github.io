dd559de13f1da1c1308434bca9f7a7bba4fae999
Michelle H <michelleh@Michelles-MacBook-Pro-2.local>
2017-11-26 12:21:10 -0800

    min reqs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,45 @@
 $(document).ready(function() {
+var aboutPos = $("#about-section").offset().top - 0;
+var projectPos = $("#projects-section").offset().top - 0;
+var contactPos = $("#contact-section").offset().top - 0;
 
-  // QUESTION 6
+$(window).load(function() {
+  // jQuery Function Number #1
+  $(".navbar-container").hide();
+})
 
-  //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
-  $("#sidebar-button").click(function(){
-    if ($(".sidebar-container").hasClass("sidebar-active")){
-      $("body").removeClass("no-scroll");
-      $("#sidebar-button").removeClass("button-active");
-      $(".sidebar-container").removeClass("sidebar-active");
-      $(".page-wrapper").removeClass("wrapper-active");
-    } else{
-      setTimeout(function() {
-        $("body").addClass('no-scroll');
-        }, 300);
-      $("#sidebar-button").addClass("button-active");
-      $(".sidebar-container").addClass("sidebar-active");
-      $(".page-wrapper").addClass("wrapper-active");
+
+//navbar appear past first section
+  $(window).scroll(function() {
+
+    // jQuery Function Number #2
+      if ($(window).scrollTop() > aboutPos) {
+
+        // jQuery Function Number #3
+        $(".navbar-container").slideDown(400);
+      } else {
+
+        // jQuery Function Number #4
+        $(".navbar-container").slideUp(400);
+      }
+
+
+      if ($(window).scrollTop() > projectPos) {
+
+        if ($(window).scrollTop() < contactPos) {
+
+          //jQuery Function Number #5
+           $(".navbar-container").addClass("navbar-dark", {duration:500});
+           //jQuery Function Number #6
+           $(".navbar-container").removeClass("navbar-light");
+
+        } else{
+          $(".navbar-container").addClass("navbar-light");
+           $(".navbar-container").removeClass("navbar-dark");
+
+        }
     }
+})
 
-  });
-
-
-  // QUESTION 7
-
-  //Implement the hiding of the sidebar when the user clicks on the page wrapper here:
-  $(".page-wrapper").click(function(){
-    if ($(".sidebar-container").hasClass("sidebar-active")){
-      $("body").removeClass("no-scroll");
-      $("#sidebar-button").removeClass("button-active");
-      $(".sidebar-container").removeClass("sidebar-active");
-      $(".page-wrapper").removeClass("wrapper-active");
-    }
-  });
-
-  // QUESTION 8
-
-  //Implement the "slide to left" when the user clicks on #carousel-next here
-  $("#carousel-next").click(function(){
-    let x = parseInt($('#carousel').css('margin-left').replace("px", "")) 
-
-    if(x != -3840){
-      $("#carousel").css("margin-left", x - 960);
-    }  
-  });
-
-  //Implement the "slide to right" when the user clicks on #carousel-prev here
-  $("#carousel-prev").click(function(){
-    let x = parseInt($('#carousel').css('margin-left').replace("px", "")) 
-
-    if(x != 0){
-      $("#carousel").css("margin-left", x + 960);
-    }
-
-    
-  });
-
-
-  //THIS IS NOT A REQUIRED QUESTION 
-  // EXTRA FOR EXPERTS 
-
-  // Implement a "smooth scroll" when the user clicks on the sidebar links here
 
 });
